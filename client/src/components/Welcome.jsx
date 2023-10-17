f3a375eb6225902a084a8701cb199b3b881a16d7
seilylook <seilylook@naver.com>
2023-10-17 20:30:21 +0900

    feat: transaction complete

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -22,10 +22,23 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-  const { connectWallet } = useContext(TransactionContext);
+  const {
+    connectWallet,
+    currentAccount,
+    formData,
+    handleChange,
+    sendTransaction,
+  } = useContext(TransactionContext);
 
-  const handleSubmit = () => {
-    alert("button clicked");
+  const handleSubmit = (e) => {
+    const { addressTo, amount, keyword, message } = formData;
+
+    e.preventDefault();
+
+    if (!addressTo || !amount || !keyword || !message)
+      return alert("Please type the fields");
+
+    sendTransaction();
   };
 
   return (
@@ -40,14 +53,18 @@ const Welcome = () => {
             Krypto.
           </p>
 
-          <button
-            type="button"
-            onClick={connectWallet}
-            className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd] w-full"
-          >
-            <AiFillPlayCircle className="text-white mr-2" />
-            <p className="text-white text-base font-semibold">Connect Wallet</p>
-          </button>
+          {!currentAccount && (
+            <button
+              type="button"
+              onClick={connectWallet}
+              className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd] w-full"
+            >
+              <AiFillPlayCircle className="text-white mr-2" />
+              <p className="text-white text-base font-semibold">
+                Connect Wallet
+              </p>
+            </button>
+          )}
 
           <div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
             <div className={`rounded-tl-2xl ${companyCommonStyles}`}>
@@ -89,25 +106,25 @@ const Welcome = () => {
               placeholder="Address To"
               name="addressTo"
               type="text"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
             <Input
               placeholder="Amount (ETH)"
               name="amount"
               type="number"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
             <Input
               placeholder="Keyword (Gif)"
               name="keyword"
               type="text"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
             <Input
               placeholder="Enter Message"
               name="message"
               type="text"
-              handleChange={() => {}}
+              handleChange={handleChange}
             />
 
             <div className="h-[1px] w-full bg-gray-400 my-2" />
--- client/src/context/TransactionContext.jsx
+++ client/src/context/TransactionContext.jsx
@@ -21,22 +21,50 @@ const getEthereumContract = () => {
     signer,
     transactionContract,
   });
+
+  return transactionContract;
 };
 
 export const TransactionProvider = ({ children }) => {
   const [currentAccount, setCurrentAccount] = useState("");
+  const [formData, setFormData] = useState({
+    addressTo: "",
+    amount: "",
+    keyword: "",
+    message: "",
+  });
+  const [isLoading, setIsLoading] = useState(false);
+  // 새로고침해도 transaction 개수를 유지하기 위해
+  // localstorage에 저장해둔다.
+  const [transactionCount, setTransactionCount] = useState(
+    localStorage.getItem("transactionCount")
+  );
+
+  const handleChange = (e, name) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: e.target.value,
+    }));
+  };
 
   const checkIfWalletIsConnected = async () => {
-    if (!ethereum) {
-      return alert("Please install metamask");
-    }
+    try {
+      if (!ethereum) {
+        return alert("Please install metamask");
+      }
 
-    const accounts = await ethereum.request({ method: "eth_accounts" });
+      const accounts = await ethereum.request({ method: "eth_accounts" });
 
-    if (accounts.length) {
-      setCurrentAccount(accounts[0]);
-    } else {
-      console.error("No accounts found");
+      console.log("현재 등록된 계좌:", accounts);
+
+      if (accounts.length) {
+        setCurrentAccount(accounts[0]);
+      } else {
+        console.error("No accounts found");
+      }
+    } catch (error) {
+      console.error(error);
+      throw new Error("No Ethereum object exist");
     }
   };
 
@@ -58,12 +86,73 @@ export const TransactionProvider = ({ children }) => {
     }
   };
 
+  const sendTransaction = async () => {
+    try {
+      if (!ethereum) return alert("Please install metamask");
+
+      // get the data from the form.
+      // after user type addressTo, amount, keyword, message,
+      const { addressTo, amount, keyword, message } = formData;
+
+      const transactionContract = getEthereumContract();
+
+      // 사용자가 입력한 0.00001과 같은 가격을 가상 화폐 기준인
+      // 16진수로 변환해주어야 한다.
+      const parsedAmount = ethers.utils.parseEther(amount);
+
+      await ethereum.request({
+        method: "eth_sendTransaction",
+        params: [
+          {
+            from: currentAccount,
+            to: addressTo,
+            gas: "0x5208", // 대략 0.000021 이더리움
+            value: parsedAmount._hex, // 0.00001 -> 16진수로 변환한 수
+          },
+        ],
+      });
+
+      const transactionHash = await transactionContract.addToBlockchain(
+        addressTo,
+        parsedAmount,
+        message,
+        keyword
+      );
+
+      console.log(transactionHash);
+
+      setIsLoading(true);
+      console.log(`Loading - ${transactionHash.hash}`);
+      setIsLoading(false);
+      console.log(`Success - ${transactionHash.hash}`);
+
+      const transactionCount = await transactionContract.getTransactionCount();
+      console.log(transactionCount);
+
+      setTransactionCount(transactionCount.toNumber());
+
+      await transactionHash.wait();
+    } catch (error) {
+      console.error(error);
+      throw new Error("No Ethereum object exist");
+    }
+  };
+
   useEffect(() => {
     checkIfWalletIsConnected();
   }, []);
 
   return (
-    <TransactionContext.Provider value={{ connectWallet }}>
+    <TransactionContext.Provider
+      value={{
+        connectWallet,
+        currentAccount,
+        formData,
+        setFormData,
+        handleChange,
+        sendTransaction,
+      }}
+    >
       {children}
     </TransactionContext.Provider>
   );
